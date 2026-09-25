1111b68389da98932c83d7cf55d0f39710c69072
celnik-213 <uT_4000@zsp3zamosc.pl>
2026-09-25 08:43:00 +0200

    zadanie6

--- MauiApp1/NewPage4.xaml.cs
+++ MauiApp1/NewPage4.xaml.cs
@@ -6,10 +6,23 @@ public partial class NewPage4 : ContentPage
 	{
 		InitializeComponent();
 	}
-
     private void Stepper_ValueChanged(object sender, ValueChangedEventArgs e)
     {
 		string value = Stepper.Value.ToString("F0");
 		StepperValue.Text = $"Osoby: {value}";
+    }
+	private void OnButtonClicked(object sender, EventArgs e)
+	{
+        string value = Stepper.Value.ToString("F0");
+        if (PickerSale.SelectedIndex == -1)
+		{
+			DisplayAlert("Błąd", "Wybierz sale", "OK");
+		}
+		else
+		{
+            string picker = PickerSale.SelectedItem.ToString();
+			DisplayAlert("Rezerwacja pomyślna", $"Sala: {picker},\n Liczba osób: {value},\n Data: {Data.Date:yyyy-MM-dd}", "OK");
+		}
+
     }
 }
